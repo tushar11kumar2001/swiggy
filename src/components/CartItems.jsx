b19fda1b82840ce459e7a022f59fe681d19b06de
tushar11kumar2001 <tushar11kumar2001@gmail.com>
2024-02-26 04:13:23 +0530

    quantity button

--- src/components/CartItems.jsx
+++ src/components/CartItems.jsx
@@ -5,31 +5,13 @@ const CartItems = ({ items }) => {
   const [count, setcount] = useState(1);
   return (
     <div>
-      <h1>quantitytushar {count}</h1>
-      <button
-        onClick={(e) => {
-          setcount(count + 1);
-        }}
-      >
-        +
-      </button>
-      <div>
-        <div
-          className={
-            "flex justify-between p-2.5 m-2 border-b-2 border-gray-400 bg-slate-100 rounded-t"
-          }
-        >
+      <div className="flex flex-col justify-between p-2.5 m-2 border-b-2 border-gray-400 bg-slate-100 rounded ">
+        <div className="flex justify-between ">
           <div>
             {items?.card?.info?.isVeg ? (
-              <img
-                className="w-4"
-                src={vegMark}
-              />
+              <img className="w-4" src={vegMark} />
             ) : (
-              <img
-                className="w-4"
-                src={nonVegMark}
-              />
+              <img className="w-4" src={nonVegMark} />
             )}
             <h2 className="font-bold text-base ">{items?.card?.info?.name}</h2>
             <p>
@@ -42,12 +24,33 @@ const CartItems = ({ items }) => {
               {items?.card?.info?.description}
             </p>
           </div>
-          <div className="relative">
-            <img
-              src={IMG_URL + items?.card?.info?.imageId}
-              className="w-36 h-28 rounded-lg  "
-            />
-          </div>
+          <img
+            src={IMG_URL + items?.card?.info?.imageId}
+            className="w-36 h-28 rounded-lg  "
+          />
+        </div>
+
+        <div className=" mt-2 pr-7">
+          <h1 className="font-semibold text-end ">
+            Quantity{" "}
+            <button
+              className="px-3 mr-2 text-2xl font-semibold text-white bg-red-600 text-center pb-1 rounded-lg"
+              onClick={(e) => {
+                setcount( count >= 2 ? count - 1 : 1);
+              }}
+            >
+              -
+            </button>
+            <span className="text-xl">{count}</span>
+            <button 
+              className="px-2 ml-2 text-2xl font-semibold text-white bg-red-600 text-center pb-1 rounded-lg"
+              onClick={(e) => {
+                setcount(count + 1);
+              }}
+            >
+              +
+            </button>
+          </h1>
         </div>
       </div>
     </div>
